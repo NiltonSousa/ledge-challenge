93d0db92876eba4cebf6294c8d44a95c65ee7309
Nilton Sousa <nsousa@zak.app>
2025-09-12 20:18:40 -0300

    refactor: refactor moviment module

--- src/application/modules/moviment.module.ts
+++ src/application/modules/moviment.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MovimentController } from '../controllers';
 import { MovimentService } from '../services';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MovimentSchema } from 'src/infra/typeorm/schemas';
+import { MovimentSchema } from '@/infra/typeorm/schemas';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MovimentSchema])],
