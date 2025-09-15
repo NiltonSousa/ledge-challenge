87585b53a329d0dd058613239001f1fd84ea9dba
Nilton Sousa <nsousa@zak.app>
2025-09-15 10:26:09 -0300

    refactor: adjust migration tables

--- src/infra/typeorm/migrations/1757702583660-create-account-moviment-table.ts
+++ src/infra/typeorm/migrations/1757702583660-create-account-moviment-table.ts
@@ -3,29 +3,31 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateAccountTable1757702583660 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-    CREATE TABLE IF NOT EXISTS public.account (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255)   NOT NULL,
-    document    VARCHAR(64)    NOT NULL,
-    email       VARCHAR(255)   NOT NULL,
-    created_at  TIMESTAMPTZ    NOT NULL DEFAULT now(),
-    updated_at  TIMESTAMPTZ    NOT NULL DEFAULT now()
-    );
+      CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-    CREATE TABLE IF NOT EXISTS public.moviment (
-    id           SERIAL PRIMARY KEY,
-    account_id   INTEGER         NOT NULL,
-    amount       NUMERIC(14,2)   NOT NULL,
-    type         VARCHAR(32)     NOT NULL,
-    description  TEXT,
-    created_at   TIMESTAMPTZ     NOT NULL DEFAULT now(),
-    updated_at   TIMESTAMPTZ     NOT NULL DEFAULT now(),
-    CONSTRAINT fk_moviment_account
-        FOREIGN KEY (account_id)
-        REFERENCES public.account(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-    );
+      CREATE TABLE IF NOT EXISTS public.account (
+        id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        name        VARCHAR(255)   NOT NULL,
+        document    VARCHAR(64)    NOT NULL,
+        email       VARCHAR(255)   NOT NULL,
+        created_at  TIMESTAMPTZ    NOT NULL DEFAULT now(),
+        updated_at  TIMESTAMPTZ    NOT NULL DEFAULT now()
+      );
+
+      CREATE TABLE IF NOT EXISTS public.moviment (
+        id           uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        account_id   uuid            NOT NULL,
+        amount       NUMERIC(14,2)   NOT NULL,
+        type         VARCHAR(32)     NOT NULL,
+        description  TEXT,
+        created_at   TIMESTAMPTZ     NOT NULL DEFAULT now(),
+        updated_at   TIMESTAMPTZ     NOT NULL DEFAULT now(),
+        CONSTRAINT fk_moviment_account
+          FOREIGN KEY (account_id)
+          REFERENCES public.account(id)
+          ON DELETE CASCADE
+          ON UPDATE CASCADE
+      );
 `);
   }
 
